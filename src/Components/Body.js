e7e92993c79ee29af07b7680410e1c92ced45d88
Rajat singh <singhrajat176@gmail.com>
2025-04-01 13:59:16 +0530

    add checked/unchecked feature and lift the state up

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -6,6 +6,13 @@ const Body = () => {
     const [desc,setDesc]=useState("");
     const [work,setWork]=useState([]);
 
+    const [totalChecked, setTotalChecked] = useState(0);
+
+    const incrementCheckedCount = () => {
+      setTotalChecked(prevCount => prevCount + 1);
+    };
+
+
     const handleSubmit=(e)=>{
         e.preventDefault();
         
@@ -15,21 +22,36 @@ const Body = () => {
         console.log(work);
 
     }
+    if(totalChecked===work.length && totalChecked!=0){
+        alert("yayy,you have done all your task.")
+    }
     
     let renderTask=<h1>No Task available</h1>
     if(work.length>0){
     renderTask=work.map((w)=>{
 
             return (
-              <Desc a={w.task} b={w.desc}/>
+             w.task.length?<Desc a={w.task} b={w.desc} fun={incrementCheckedCount}/>:<></>
         )
     })
+}
+const handleClear=()=>{
+    if(work.length!=0){
+
+        alert("Do you want to delete your tasks?")
+        setWork([]);
+        setTotalChecked(0);
+    }
+    else {
+        alert("No task available")
+
+    }
 }
 
   return (
    
    <>
-   <div className='my-12 py-12 bg-orange-500 opacity-80'>
+   <div className='my-8 py-12 bg-orange-500 opacity-80'>
     <h1 className='text-center font-bold text-white'>To Do</h1>
     <div>
         <div className='flex justify-center'>
@@ -37,13 +59,14 @@ const Body = () => {
     <form onSubmit={handleSubmit} className='border-2 border-white w-6/12 p-4 '>
     <input type='text' placeholder='Task' className='w-1/2  mt-2 p-2' value={task} onChange={(e)=>{setTask(e.target.value)}} ></input>
     <input type='text' placeholder='Describe' className='w-3/4 my-2 p-2'  value={desc} onChange={(e)=>{setDesc(e.target.value)}}></input>
-    <button className='w-24 h-9 ml-4 bg-black text-white rounded-md'> Add</button>
+    <button className='w-24 h-9 ml-4 bg-black text-white rounded-md hover:bg-gray-800'> Add</button>
     </form> 
         </div>
     </div>
     
    </div>
-    {/* {work.length>0?<div>task</div>:<></>} */}
+   <button className='left-[25px] relative mb-5 text-white bg-black  px-3 py-1 rounded-sm' onClick={handleClear}>Clear</button><hr></hr>
+   {/* <div>{totalChecked}</div> */}
     {renderTask}
    </>
   )
--- src/Components/Desc.js
+++ src/Components/Desc.js
@@ -1,18 +1,33 @@
 import React, { useState } from 'react'
 
-const Desc = ({a,b}) => {
+const Desc = ({a,b,fun}) => {
    
     const [show,setShow]=useState(false);
     const [icon,setIcon]=useState("⬇");
+    const [box,setBox]=useState("☑️");
+    const [color,setColor]=useState("gray");
 
     const handleShow=()=>{
         show?setIcon("⬇"):setIcon("⬆")
         setShow(!show)
     }
+    const handleBox=()=>{
+      if(color=="gray"){
+     
+        alert("Make sure you have done this task!")
+        setBox("✅")
+        setColor("green");
+        fun();
+      }
+    }
   return (
-    <div className=' m-2 w-8/12  text-3xl pl-2 pr-8 py-1'>
-    <div className='flex justify-between bg-cyan-400 w-1/3 rounded-lg'><div className=' text-white w-4/6 py-1 px-3 rounded-md'>{a}</div><div className='pr-3 cursor-pointer text-orange-400 text-sm pt-3' onClick={handleShow}>{icon}</div></div>
-   { show && <div className= ' text-black border-l-4 border-b-2 border-orange-400 w-3/4 mt-4 rounded-sm px-3 pt-6 pb-3 text-lg'>{b}</div>}
+  <div className=' m-3 w-8/12  text-3xl pl-2 pr-8 py-1'>
+    <div className='flex justify-between bg-cyan-400 w-1/3 rounded-lg'>
+     <div className=' text-white w-4/6 py-1 px-3 rounded-md'>{a}</div>
+     <div className='pr-3 cursor-pointer left-12 relative text-orange-400 text-sm pt-3' onClick={handleShow}>{icon}</div>
+     <div className='relative left-12 top-1 cursor-pointer' onClick={handleBox}>{box}</div>
+    </div>
+   { show && <div className= ' text-black border-l-2 border-b-2 border-orange-400 w-3/4 mt-0 rounded-sm px-3 pt-7 pb-9 text-lg'>{b}</div>}
 </div>
   )
 }
